61435ef7ab61cbd3265a1f11b16efa09d18cafee
Supratim <supratim.gre2014@yahoo.com>
2019-08-12 00:11:42 +0200

    more utis added

--- util_functions.py
+++ util_functions.py
@@ -5,6 +5,10 @@ from boto3.s3.transfer import S3Transfer
 import boto3
 import configparser
 import logging
+from pyspark.sql import SparkSession
+from os import listdir
+from os.path import isfile, join
+from pyspark.sql.types import *
 
 
 # to be moved later
@@ -118,3 +122,99 @@ def transfer_files_to_s3(input_path, bucket_name, file_ext):
                 logging.info("transferring file {}".format(file_name))
                 transfer.upload_file(full_path, bucket_name, file_ext
                                      + '/' + file)
+
+
+def create_dataframe_from_sas(filepath):
+    """
+    This function reads all the sas formatted files and returns a final
+    dataframe containing all the required and consolidated rows as a
+    pandas dataframe
+    :param filepath:
+    :return: df_all
+    """
+    # spark session
+    logging.info("creating spark session ...")
+    spark = SparkSession.builder \
+                        .config("spark.jars.packages",
+                                "saurfang:spark-sas7bdat:2.0.0-s_2.11")\
+                        .enableHiveSupport() \
+                        .getOrCreate()
+
+    # spark context
+    sc = spark.sparkContext
+
+    # column names
+    logging.info("defining column names and resulting schema ...")
+    columns = ['cicid',
+               'i94yr',
+               'i94mon',
+               'i94cit',
+               'i94res',
+               'i94port',
+               'arrdate',
+               'i94mode',
+               'i94addr',
+               'depdate',
+               'i94bir',
+               'i94visa',
+               'count',
+               'dtadfile',
+               'visapost',
+               'occup',
+               'entdepa',
+               'entdepd',
+               'entdepu',
+               'matflag',
+               'biryear',
+               'dtaddto',
+               'gender',
+               'insnum',
+               'airline',
+               'admnum',
+               'fltno',
+               'visatype']
+
+    # schema definition
+    schema = StructType([
+        StructField('cicid', DoubleType(), True),
+        StructField('i94yr', DoubleType(), True),
+        StructField('i94mon', DoubleType(), True),
+        StructField('i94cit', DoubleType(), True),
+        StructField('i94res', DoubleType(), True),
+        StructField('i94port', StringType(), True),
+        StructField('arrdate', DoubleType(), True),
+        StructField('i94mode', DoubleType(), True),
+        StructField('i94addr', StringType(), True),
+        StructField('depdate', DoubleType(), True),
+        StructField('i94bir', DoubleType(), True),
+        StructField('i94visa', DoubleType(), True),
+        StructField('count', DoubleType(), True),
+        StructField('dtadfile', StringType(), True),
+        StructField('visapost', StringType(), True),
+        StructField('occup', StringType(), True),
+        StructField('entdepa', StringType(), True),
+        StructField('entdepd', StringType(), True),
+        StructField('entdepu', StringType(), True),
+        StructField('matflag', StringType(), True),
+        StructField('biryear', DoubleType(), True),
+        StructField('dtaddto', StringType(), True),
+        StructField('gender', StringType(), True),
+        StructField('insnum', StringType(), True),
+        StructField('airline', StringType(), True),
+        StructField('admnum', DoubleType(), True),
+        StructField('fltno', StringType(), True),
+        StructField('visatype', StringType(), True)
+    ])
+
+    df_all = spark.createDataFrame(sc.emptyRDD(), schema)
+
+    onlyfiles = [join(filepath, f) for f in listdir(filepath) if
+                 isfile(join(filepath, f))]
+
+    logging.info("reading files from the disc ... ")
+    for f in onlyfiles:
+        df_temp = spark.read.format(
+            'com.github.saurfang.sas.spark').load(f).select(columns)
+        df_all = df_all.union(df_temp)
+
+    return df_all.toPandas()
